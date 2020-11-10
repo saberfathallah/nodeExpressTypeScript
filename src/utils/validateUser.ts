12f9962e928bb7d21d23e222a62ff16e9270bdd7
saber <saberfathallah7@gmail.com>
2020-11-10 14:18:09 +0100

    err

--- src/utils/validateUser.ts
+++ src/utils/validateUser.ts
@@ -5,7 +5,7 @@ const validateUser = (req: Request, res: Response): void => {
   const { userid } = req.headers;
 
   if (!userid) {
-    res.status(422).json({ error: 'error authentification' });
+    res.status(401).json({ error: 'error authentification' });
     throw new Error('error authentification');
   }
 };
